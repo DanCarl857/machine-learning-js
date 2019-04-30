d5f7cd2eb9894f74c99d055a9b8e1bc6f198fb4d
Daniel Carlson <dancarl758@gmail.com>
2019-04-30 13:46:27 +0100

    Added code for better predictions

--- MLKits-master/plinko/score.js
+++ MLKits-master/plinko/score.js
@@ -7,9 +7,29 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  // kNN algorithm implementation
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+
+  // let numberCorrect = 0;
+  // for(let i = 0; i < testSet.length; i++) {
+  //   const bucket = kNN(trainingSet, testSet[i][0]);
+  //   if (bucket == testSet[i][3]) {
+  //     numberCorrect++;
+  //   }
+  // }
+
+  const accuracy = _.chain(testSet)
+    .filter(testPoint => kNN(trainingSet, testPoint[0]) === testPoint[3])
+    .size()
+    .divide(testSetSize)
+    .value();
+
+  console.log('Accuracy is: ', accuracy);
+}
+
+function kNN(data, point) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
     .slice(0,k)
     .countBy(row => row[1])
@@ -18,11 +38,18 @@ function runAnalysis() {
     .last()
     .first()
     .parseInt()
-    .value()
-
-  console.log({ bucket });
+    .value();
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
 }
